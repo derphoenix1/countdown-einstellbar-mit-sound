0a37feeecf91c8423aa5de94a2eff19df5bc84db
Alexander <alexandermeier2020@outlook.de>
2025-01-11 20:09:04 +0100

    Jetzt wird die Sekundenanzahl bei der Taste B um 1 niedriger; bei der Taste A+B wird der Countdown gestartet

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     startwert += 1
     basic.showNumber(startwert)
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     while (startwert != 0) {
         basic.pause(1000)
         startwert += -1
@@ -10,6 +10,10 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         basic.showNumber(startwert)
     }
     music.playTone(494, music.beat(BeatFraction.Double))
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    startwert += -1
+    basic.showNumber(startwert)
 })
 let startwert = 0
 startwert = 1
